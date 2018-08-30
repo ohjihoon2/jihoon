5d0e754cd1ed24a8fcac72a5aa8c835f42833e06
user35 <user35@gntp-35.gntp.local>
2018-08-30 16:21:42 +0900

    2018.08.30 교과Test

---
+++ addTest/addTest/src/main/java/com/asvals/addTest/adder.java
@@ -0,0 +1,13 @@
+package com.asvals.addTest;
+
+
+/**
+ * A dummy test-case to demonstrate how to use a test-suite.
+ * 
+ * @version $Id: TestCaseB.java 552 2010-03-06 11:48:47Z paranoid12 $
+ */
+public class adder {
+	int addition(int num1, int num2){
+		return num1+num2;
+	}
+}
